f3b58b7eece62dddf301ad13b9cab0e11e43b122
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-01-01 22:25:53 +0100

    feat: enhance Box component to support additional motion props

--- package/src/components/Box/Box.tsx
+++ package/src/components/Box/Box.tsx
@@ -2,12 +2,14 @@ import { getDisplayName } from "../../utils/displayname";
 import { forwardRef } from "react";
 import { useBox } from "./use-box";
 import { BoxWithMotion } from "../../types/auera-motion";
+import { useProps } from "@/utils";
 
 const Box = forwardRef<HTMLDivElement, BoxWithMotion>(
   ({ children, ...props }, ref) => {
     const { Base } = useBox(props);
+    const Props = useProps(props, "div");
     return (
-      <Base ref={ref} {...props}>
+      <Base ref={ref} {...Props}>
         {children}
       </Base>
     );
