523485dbf3366879a024c8dbb4b639717b52971d
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-01-03 01:20:52 +0100

    docs(DrawerContent): enhance styling for dark mode

--- docs/src/components/example/DrawerContent.tsx
+++ docs/src/components/example/DrawerContent.tsx
@@ -16,21 +16,23 @@ const Drawer_Content = () => {
           direction="column"
           className="
         gap-2 p-4 rounded-xl cursor-pointer 
-        border border-neutral-200 
+        border border-neutral-200 theme-dark:border-neutral-700
         hover:bg-gradient-to-r hover:from-indigo-50 hover:to-purple-50 
         transition-all duration-300 ease-in-out shadow-md 
-        hover:shadow-lg transform hover:scale-105 mb-2 active:scale-90
+        hover:shadow-lg transform hover:scale-105 mb-2 active:scale-90 drawer-demo-card
       "
         >
           <Box fullWidth className="flex justify-between items-center">
-            <p className="font-semibold text-lg text-neutral-800">
+            <p className="font-semibold text-lg text-neutral-800 theme-dark:text-white">
               {item.title}
             </p>
             {!item.seen && (
               <StatusBadge badgeClassName="" placement="right-bottom" />
             )}
           </Box>
-          <span className="text-neutral-600 text-sm">{item.message}</span>
+          <span className="text-neutral-600 text-sm theme-dark:text-neutral-500">
+            {item.message}
+          </span>
         </Box>
       )}
       groupByKey={(item) => item.timestamp}
