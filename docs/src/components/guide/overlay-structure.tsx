0867d0358fd5030109df791f0c420d7c29b35271
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-01-03 02:55:00 +0100

    feat(docs): enhance OverlayStructure documentation with new examples and code snippets

--- docs/src/components/guide/overlay-structure.tsx
+++ docs/src/components/guide/overlay-structure.tsx
@@ -2,19 +2,86 @@ import React from "react";
 import StepGuide from "../StepGuide";
 import GuideContainer from "../layout/GuideContainer";
 import CodeTag from "../CodeTag";
+import HeaderLabel from "../HeaderLabel";
+import CodeBlock from "../layout/code/Block";
+import { overlayCodes } from "@/data/code/usage.overlay";
 
 const OverlayStructure = () => {
   return (
     <>
-      <StepGuide step={1} id="create-file">
+      <StepGuide step={1} id="overlay-component">
+        <GuideContainer
+          direction="column"
+          id="overlay-component"
+          fullWidth
+          className="gap-4"
+        >
+          <HeaderLabel
+            id="overlay-component"
+            as="h3"
+            className="text-lg"
+            hideLink
+          >
+            Overlay Component
+          </HeaderLabel>
+          <p className="text">
+            Below is an example of how you should structure your overlays.
+          </p>
+          <CodeBlock
+            fileName="components/overlays/auth.tsx"
+            lg="tsx"
+            code={overlayCodes.componentFile}
+          />
+        </GuideContainer>
+      </StepGuide>
+
+      <StepGuide step={2} id="create-file">
         <GuideContainer
           direction="column"
           id="create-file"
           fullWidth
           className="gap-4"
         >
-          Create a single file, such as <CodeTag>overlays/container.js</CodeTag>{" "}
-          or <CodeTag>overlays/container.tsx</CodeTag>
+          <HeaderLabel id="create-file" as="h3" className="text-lg" hideLink>
+            Create File
+          </HeaderLabel>
+          <p className="text">
+            Create a single file, such as{" "}
+            <CodeTag>overlays/container.js</CodeTag> or{" "}
+            <CodeTag>overlays/container.tsx</CodeTag>, and import all the
+            overlay components into it.
+          </p>
+          <CodeBlock
+            fileName="components/overlays/container.tsx"
+            lg="tsx"
+            code={overlayCodes.container}
+          />
+        </GuideContainer>
+      </StepGuide>
+
+      <StepGuide step={3} id="import" removeTrack>
+        <GuideContainer
+          direction="column"
+          id="import"
+          fullWidth
+          className="gap-4"
+        >
+          <HeaderLabel id="import" as="h3" className="text-lg" hideLink>
+            Import Components
+          </HeaderLabel>
+          <p className="text">
+            Import the newly created Overlays Component from{" "}
+            <CodeTag>overlays/container.tsx</CodeTag> into your root layout or{" "}
+            <CodeTag>App.tsx</CodeTag>.
+          </p>
+          <CodeBlock
+            fileName="layout.tsx"
+            lg="tsx"
+            code={overlayCodes.layout}
+          />
+          <p className="text">
+            This approach simplifies imports and centralizes overlay management.
+          </p>
         </GuideContainer>
       </StepGuide>
     </>
---
+++ docs/src/data/code/usage.overlay.ts
@@ -0,0 +1,56 @@
+export const overlayCodes = {
+  componentFile: `import { Modal, ModalPanel, ModalContent, ModalHeader, IconButton } from "auera-ui";
+import { IoClose } from "react-icons/io5";
+
+const AuthModal = () => {
+  // Your logic here
+  return (
+    <Modal value="auth-modal">
+        <ModalPanel>
+          <ModalHeader>
+            <h1>Sign in</h1>
+            <IconButton
+              withTrigger
+              trigger="modal"
+              triggerType="close"
+              triggerValue="auth-modal"
+              variants="ghost"
+            >
+              <IoClose size={18} />
+            </IconButton>
+          </ModalHeader>
+          <ModalContent>{/* Add your modal content here */}</ModalContent>
+        </ModalPanel>
+      </Modal>
+  );
+};
+  
+export default AuthModal;`,
+  container: `import AuthModal from "./auth";
+import MenuDrawer from "./menu";
+
+const Overlays = () => {
+    return (
+      <>
+        <AuthModal />
+        <MenuDrawer />
+        {/* Add more overlays as needed */}
+      </>
+    );
+};
+
+export default Overlays;
+  `,
+  layout: `import Overlays from "../components/overlays/container";
+  
+const RootLayout = () => {
+  return (
+    <>
+    {/* Other components */}
+    <Overlays />
+    </>
+  );
+};
+  
+export default RootLayout;`,
+};
