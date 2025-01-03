848e7a305bd70ea8c6a2ca667b6660b62c186cd5
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-01-03 01:13:03 +0100

    refactor(Button): update code examples to include withTrigger functionality

--- docs/src/components/example/button/with-trigger.tsx
+++ docs/src/components/example/button/with-trigger.tsx
@@ -24,7 +24,7 @@ const ButtonWithTrigger = () => {
         <TabPanel>
           <CodeBlock
             fileName="with-trigger.tsx"
-            code={buttonCode.loading}
+            code={buttonCode.withTrigger}
             lg="tsx"
           />
         </TabPanel>
