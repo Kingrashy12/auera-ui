dffbafa3d1df5da19ba7fe5cc8aabc70a19b7694
Rapheal Chizitere <www.rashyking05@gmail.com>
2025-01-18 19:43:25 +0100

    docs: update components list

--- package/src/components/Button/Fab.tsx
+++ package/src/components/Button/Fab.tsx
@@ -9,7 +9,7 @@ const fabPosition = {
 };
 
 const colorScheme = {
-  primary: "bg-purple-500 text-white hover:bg-purple-600",
+  primary: "bg-blue-500 text-white hover:bg-blue-600",
   bold: "bg-black text-white hover:bg-gray-800",
   soft: "bg-white text-black hover:bg-gray-100",
 };
